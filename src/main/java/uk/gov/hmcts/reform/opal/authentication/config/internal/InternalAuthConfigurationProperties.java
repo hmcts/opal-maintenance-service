93fd62d52770b6b52a3c271055958f609ee9f842
Max Holland <Max.Holland@HMCTS.NET>
2026-08-19 19:11:11 +0100

    fix(auth): reject blank client id

--- src/main/java/uk/gov/hmcts/reform/opal/authentication/config/internal/InternalAuthConfigurationProperties.java
+++ src/main/java/uk/gov/hmcts/reform/opal/authentication/config/internal/InternalAuthConfigurationProperties.java
@@ -1,17 +1,21 @@
 package uk.gov.hmcts.reform.opal.authentication.config.internal;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 import uk.gov.hmcts.opal.common.user.authentication.config.AuthConfigurationProperties;
 
 @Component
 @ConfigurationProperties("spring.security.oauth2.client.registration.internal-azure-ad")
+@Validated
 @Getter
 @Setter
 public class InternalAuthConfigurationProperties implements AuthConfigurationProperties {
 
+    @NotBlank(message = "Internal AAD client ID must be configured")
     private String clientId;
     private String clientSecret;
     private String scope;
