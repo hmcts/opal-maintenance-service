7a5ef1bdc8bd7af468b6ef7642138c450eb26a47
Frankie Moran <frankie.jack.moran@gmail.com>
2026-08-26 14:16:52 +0100

    PO-10251 map country reference data

---
+++ src/main/java/uk/gov/hmcts/reform/opal/mapper/CountryMapper.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.opal.mapper;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.opal.generated.model.CountryReferenceDataItem;
+import uk.gov.hmcts.reform.opal.entity.CountryEntity;
+
+@Component
+public class CountryMapper {
+
+    public CountryReferenceDataItem toReferenceDataItem(CountryEntity country) {
+        return CountryReferenceDataItem.builder()
+            .countryId(country.getCountryId())
+            .cjsCode(country.getCjsCode().intValue())
+            .internationalCode(country.getInternationalCode())
+            .govCode(country.getGovCode())
+            .countryName(country.getCountryName())
+            .demonym(country.getDemonym())
+            .dateUsedFrom(country.getDateUsedFrom())
+            .dateUsedTo(country.getDateUsedTo())
+            .active(country.getActive())
+            .build();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/opal/mapper/CountryMapperTest.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.opal.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.opal.entity.CountryEntity;
+
+@DisplayName("PO-10251 CountryMapper")
+class CountryMapperTest {
+
+    private final CountryMapper mapper = new CountryMapper();
+
+    @Test
+    @DisplayName("PO-10251 maps all nine country fields")
+    void mapsAllCountryFields() {
+        CountryEntity entity = CountryEntity.builder()
+            .countryId(41L)
+            .cjsCode((short) 32001)
+            .internationalCode("GBR")
+            .govCode("UK")
+            .countryName("United Kingdom")
+            .demonym("British")
+            .dateUsedFrom(LocalDate.of(1900, 1, 1))
+            .dateUsedTo(LocalDate.of(2099, 12, 31))
+            .active(true)
+            .build();
+
+        var item = mapper.toReferenceDataItem(entity);
+
+        assertThat(item.getCountryId()).isEqualTo(41L);
+        assertThat(item.getCjsCode()).isEqualTo(32001);
+        assertThat(item.getInternationalCode()).isEqualTo("GBR");
+        assertThat(item.getGovCode()).isEqualTo("UK");
+        assertThat(item.getCountryName()).isEqualTo("United Kingdom");
+        assertThat(item.getDemonym()).isEqualTo("British");
+        assertThat(item.getDateUsedFrom()).isEqualTo(LocalDate.of(1900, 1, 1));
+        assertThat(item.getDateUsedTo()).isEqualTo(LocalDate.of(2099, 12, 31));
+        assertThat(item.getActive()).isTrue();
+    }
+
+    @Test
+    @DisplayName("PO-10251 preserves nullable optional country fields")
+    void preservesNullableOptionalFields() {
+        CountryEntity entity = CountryEntity.builder()
+            .countryId(42L)
+            .cjsCode((short) 32002)
+            .countryName("Atlantis")
+            .dateUsedFrom(LocalDate.of(2000, 1, 1))
+            .active(false)
+            .build();
+
+        var item = mapper.toReferenceDataItem(entity);
+
+        assertThat(item.getInternationalCode()).isNull();
+        assertThat(item.getGovCode()).isNull();
+        assertThat(item.getDemonym()).isNull();
+        assertThat(item.getDateUsedTo()).isNull();
+    }
+}
