a089e8f00c933de2a57c6c0ca33c85d855dfe5c2
Frankie Moran <frankie.jack.moran@gmail.com>
2026-08-28 13:48:43 +0100

    test(database): add pgtap integration harness

--- src/integrationTest/java/uk/gov/hmcts/reform/opal/BaseIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/opal/BaseIntegrationTest.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.reform.opal;
 
+import java.nio.file.Path;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -13,7 +16,15 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 @SuppressWarnings("HideUtilityClassConstructor")
 public class BaseIntegrationTest {
 
+    private static final ImageFromDockerfile POSTGRES_PGTAP_IMAGE = new ImageFromDockerfile()
+        .withFileFromPath(
+            "Dockerfile",
+            Path.of("docker/postgres-pgtap.Dockerfile").toAbsolutePath()
+        );
+
     @ServiceConnection
     @Container
-    static PostgreSQLContainer databaseContainer = new PostgreSQLContainer("postgres:17");
+    protected static PostgreSQLContainer databaseContainer = new PostgreSQLContainer(
+        DockerImageName.parse(POSTGRES_PGTAP_IMAGE.get()).asCompatibleSubstituteFor("postgres")
+    );
 }
---
+++ src/integrationTest/java/uk/gov/hmcts/reform/opal/database/PgTapTestRunnerIntegrationTest.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.opal.database;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.opal.BaseIntegrationTest;
+
+class PgTapTestRunnerIntegrationTest extends BaseIntegrationTest {
+
+    private static final Path FAILING_PGTAP_TEST =
+        Path.of("src/integrationTest/resources/db/pgtap/failing_pgtap_test.sql");
+
+    @Test
+    void failedPgTapAssertionFailsTheRunner() {
+        assertThatThrownBy(() -> PgTapTestRunner.run(databaseContainer, FAILING_PGTAP_TEST))
+            .isInstanceOf(AssertionError.class)
+            .hasMessageContaining("pg_prove failed for")
+            .hasMessageContaining("deliberate pgTAP failure used to verify the Java runner");
+    }
+}
